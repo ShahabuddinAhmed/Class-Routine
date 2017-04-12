a86b56692d76f0cc3f1b3874533b77f4d230c44b
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2017-04-12 12:21:18 +0600

    Session is added

--- dblogin.php
+++ dblogin.php
@@ -1,36 +1,42 @@
 <?php
-
-	$stid = $_POST['id'];
-	$stpass = $_POST['pass'];
-	if($stid!="" && $stpass!= "")
+	session_start();
+	if(isset($_POST['submit']))
 	{
-		if($stid == "admin" && $stpass == "admin")
+		$stid = $_POST['id'];
+		$stpass = $_POST['pass'];
+
+		if($stid!="" && $stpass!= "")
 		{
-			header('location:admin.php');
-		}
-		else
-		{
-			include 'dbconnect.php';
-			$sql = "SELECT * FROM student WHERE ID = '".$stid."' && Pass = '".$stpass."'";
-			$result = $conn->query($sql);
-			
-			if($result->num_rows > 0)
+			if($stid == "admin" && $stpass == "admin")
 			{
-				header('location:index.php');
+				$_SESSION['login_admin']=$stid;
+				header('location:admin.php');
 			}
 			else
 			{
-				echo "Wrong Student ID or Password";
-				//header('location:login.php');
+				include 'dbconnect.php';
+				$sql = "SELECT * FROM student WHERE ID = '".$stid."' && Pass = '".$stpass."'";
+				$result = $conn->query($sql);
 				
+				if($result->num_rows > 0)
+				{
+					$_SESSION['login_user']=$stid;
+					header('location:index.php');
+				}
+				else
+				{
+					echo "Wrong Student ID or Password";
+					//header('location:login.php');
+					
+				}
 			}
 		}
-	}
-	else
-	{
-		echo "Please Fill up all information";
-	}
+		else
+		{
+			echo "Please Fill up all information";
+		}
 
-	$conn->close();
-
+		$conn->close();
+	}
+	
  ?>
